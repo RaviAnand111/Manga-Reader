5c45367a9d86320abc793fad55e4c6a3af62dc6b
RaviAnand-str8bat <ravi.anand@str8bat.com>
2025-10-27 00:56:13 +0530

    Add feature to stop scrolling for all pages but active tab on tab switch

--- background.js
+++ background.js
@@ -1,5 +1,33 @@
 
-browser.tabs.onActivated.addListener((activeInfo) => {
+browser.tabs.onActivated.addListener(async (tabInfo) => {
+  const { approvedTabs = [] } = await browser.storage.session.get('approvedTabs');
+  const isApproved = approvedTabs.includes(tabInfo.tabId);
+
+  if (isApproved) {
+    const { delay, length } = await browser.storage.local.get(['delay', 'length']);
+
+    browser.tabs.sendMessage(
+      tabInfo.tabId,
+      {
+        from: 'background',
+        action: 'start',
+        delay,
+        length
+      }
+    )
+  }
+
+  approvedTabs.forEach((tabId) => {
+    if (tabId !== tabInfo.tabId) {
+      browser.tabs.sendMessage(
+        tabId,
+        {
+          from: 'background',
+          action: 'stop',
+        }
+      )
+    }
+  })
 })
 
 browser.contextMenus.create({
@@ -13,7 +41,7 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
       target: {
         tabId: tab.id
       },
-      files: ["page-eater.js"],
+      files: ["content-script.js"],
     });
   }
 });
