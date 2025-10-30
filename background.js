2976abe211672c2a3a6c1b59f62d9410b091b268
RaviAnand-str8bat <ravi.anand@str8bat.com>
2025-10-30 21:40:40 +0530

    added dark theme

--- background.js
+++ background.js
@@ -29,19 +29,3 @@ browser.tabs.onActivated.addListener(async (tabInfo) => {
     }
   })
 })
-
-browser.contextMenus.create({
-  id: "eat-page",
-  title: "Eat this page",
-});
-
-browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "eat-page") {
-    browser.scripting.executeScript({
-      target: {
-        tabId: tab.id
-      },
-      files: ["content-script.js"],
-    });
-  }
-});
